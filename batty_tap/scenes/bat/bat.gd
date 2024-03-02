114fa506621279f505ac8e68e0f9dacdb5116dcd
Lázaro Menezes <lazarodm@gmail.com>
2024-03-01 23:46:32 -0300

    Add bat movement

---
+++ batty_tap/scenes/bat/bat.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var anti_gravity = 500
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("flap"):
+		velocity.y = -anti_gravity
+		
+	move_and_slide()
