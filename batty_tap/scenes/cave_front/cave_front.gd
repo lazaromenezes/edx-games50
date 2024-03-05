3590e1e11d4d00570bacd42cbf5ea3be744964cf
Lazaro Menezes <lazarodm@gmail.com>
2024-03-04 21:59:04 -0300

    Add up and bottom collision

--- batty_tap/scenes/cave_front/cave_front.gd
+++ batty_tap/scenes/cave_front/cave_front.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal hit()
+
 const MOVE_SPEED: float = 120
 const INITIAL_POSITION: float = 998
 const LOOPING_POINT: float = 0
@@ -9,3 +11,6 @@ func _process(delta: float) -> void:
 	
 	if $CaveSprite.position.x < LOOPING_POINT:
 		$CaveSprite.position.x = INITIAL_POSITION
+
+func _on_hit(body: Node2D):
+	hit.emit()
